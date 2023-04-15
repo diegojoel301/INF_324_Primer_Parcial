c588c0e835253bfa47725f87a68d55dee127d055
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-04-15 10:40:58 -0400

    Update bd_asp.sql

--- bd_asp.sql
+++ bd_asp.sql
@@ -27,12 +27,20 @@ CREATE TABLE PERSONA(ci nvarchar(50) PRIMARY KEY, nombre_completo nvarchar(255),
 
 GO
 
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('47365646', 'Paul Sheldon', '1987-09-11','+1 263554654345', 'SC');
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('68362354', 'Jose Genaro Chauca Pardo', '2002-02-24', '+591 64635354', 'CB');
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('72546264', 'Roger Alejandro Guarachi Mamani', '2002-09-11', '+591 74545344', 'LP');
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('57326465', 'Humberto Rojas Padilla', '2004-11-12', '+591 24645544', 'TJ');
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('12635454', 'Carlos Jahuira', '2002-11-12', '+591 64545344', 'LP');
-INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('23125353', 'Victor Andrade', '2002-03-10', '+591 60643003', 'LP');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('47365646', 'Paul Sheldon', '1987-09-11','+1 263554654345', '03');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('68362354', 'Jose Genaro Chauca Pardo', '2002-02-24', '+591 64635354', '05');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('72546264', 'Roger Alejandro Guarachi Mamani', '2002-09-11', '+591 74545344', '05');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('57326465', 'Humberto Rojas Padilla', '2004-11-12', '+591 24645544', '04');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('12635454', 'Carlos Jahuira', '2002-11-12', '+591 64545344', '02');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('23125353', 'Victor Andrade', '2002-03-10', '+591 60643003', '03');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('86545636', 'Andres Quintana', '2003-05-12', '+591 674356455', '01');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('87863444', 'Humberto Rojas Padilla', '2004-11-12', '+591 24645544', '02');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('58372644', 'Joel Ramirez', '2002-11-12', '+591 64545344', '01');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('12353454', 'Jhoel Legua', '2002-03-10', '+591 34573455', '02');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('23421523', 'Fernando Cuevas', '2003-05-12', '+591 234523524', '01');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('64383645', 'Antonio Padilla', '2005-03-12', '+591 24645544', '04');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('36466366', 'Jose Ramirez', '2004-02-12', '+591 37462254', '03');
+INSERT INTO PERSONA(ci, nombre_completo, fecha_nacimiento, telefono, departamento) VALUES ('45634655', 'Hernando Garcia', '2003-01-10', '+591 46534755', '04');
 
 GO
 
@@ -43,5 +51,13 @@ GO
 INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('23125353', 'INF-324', 100, 100, 100, 100);
 INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('57326465', 'INF-319', 56, 78, 60, 75);
 INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('12635454', 'INF-273', 67, 59, 79, 80);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('86545636', 'INF-273', 56, 70, 12, 71);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('87863444', 'INF-131', 66, 60, 32, 81);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('58372644', 'INF-121', 76, 50, 42, 62);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('12353454', 'INF-111', 86, 40, 62, 73);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('23421523', 'INF-324', 96, 40, 82, 74);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('64383645', 'INF-324', 36, 30, 92, 75);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('36466366', 'INF-319', 46, 20, 42, 76);
+INSERT INTO INSCRIPCION(ci_estudiante, sigla, nota1, nota2, nota3, notafinal) VALUES ('45634655', 'INF-324', 56, 70, 52, 87);
 
 GO
