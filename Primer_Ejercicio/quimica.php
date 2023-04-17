fbc9529cd8acc99f98802483c1df29997b9e2d9e
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-04-17 14:49:38 -0400

    Update quimica.php

--- Primer_Ejercicio/quimica.php
+++ Primer_Ejercicio/quimica.php
@@ -1,3 +1,4 @@
 <h2>Carrera de Quimica</h2>
+<img src="img/quimica.jpg">
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eius quas, quisquam, eos harum sequi inventore cumque iste dolorum qui natus, est enim magni libero sint. Harum repudiandae rerum ab.</p>
 <a href="?page=/">Volver</a>
