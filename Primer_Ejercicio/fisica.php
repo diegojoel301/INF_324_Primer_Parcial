5f1058a26a2b5e0decd22ca67827da2ed2c44ecc
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-04-17 14:47:03 -0400

    Update fisica.php

--- Primer_Ejercicio/fisica.php
+++ Primer_Ejercicio/fisica.php
@@ -1,4 +1,5 @@
 <h2>Carrera de Fisica</h2>
+<img src="img/fisica.jpg">
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eius quas, quisquam, eos harum sequi inventore cumque iste dolorum qui natus, est enim magni libero sint. Harum repudiandae rerum ab.</p>
 <a href="?page=/">Volver</a>
 
